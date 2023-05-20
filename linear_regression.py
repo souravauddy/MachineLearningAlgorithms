487eb2bbd54cb3db804b3f1e31750292efadc38d
souravauddy <souravauddy25@gmail.com>
2023-05-20 14:01:17 +0530

    first commit

---
+++ linear_regression.py
@@ -0,0 +1,125 @@
+from __future__ import annotations
+from typing import Self
+import numpy as np
+import sys
+
+"""
+N = No of features
+M = No of rows
+time complexity: O(N ^ 2 * M)
+space complexity = O(size of the dataset)
+"""
+
+
+SEED: int = 42
+SIZE: int = 100
+LEARNING_RATE: float = 0.1   # learning rate
+EPOCHS: int = 1000
+
+
+class LinearRegression(object):
+    def __init__(self, eta: float = LEARNING_RATE, epochs: int = EPOCHS) -> None:
+        self.LEARNING_RATE = eta
+        self.EPOCHS = epochs
+
+    def fit(self, X_data: np.ndarray, y_data: np.ndarray) -> Self:
+        self.X_data = X_data
+        self.y_data = y_data
+        self.size = len(X_data[0]) + 1
+        self.weights = np.random.randn(self.size)   # intialze with some random weights
+        self._gradient_descent()
+        return self
+    
+    @property
+    def weights_(self) -> np.ndarray[float]:
+        return self.weights.copy()
+    
+    def _partial_derivative(self, weight_index: int) -> float:
+        root_mean_squared_error = self._cost_function()
+        derivative = 0
+
+        for index, row in enumerate(self.X_data):
+            term = self.weights[0]
+            CO_EFFICIENT = 1 if weight_index == 0 else row[weight_index - 1]
+
+            for i, x in enumerate(row, start=1):
+                term += self.weights[i] * x
+
+            term -= self.y_data[index] 
+            term *= CO_EFFICIENT
+            derivative += term
+
+        return (derivative / len(self.y_data)) / root_mean_squared_error
+
+    def _gradient_descent(self) -> None:
+        for epoch in range(self.EPOCHS):
+            self.weights = self.weights - self.LEARNING_RATE * self._gradient()
+            print(f"{epoch=}, {self.weights=}")
+
+        """
+        # another approach using keyboard interupt for unlimited epochs
+        best_weights = None
+        best_cost = sys.maxsize
+        epoch = 1
+
+        try:
+            while True:
+                self.weights = self.weights - self.LEARNING_RATE * self._gradient()
+                cost = self._cost_function()
+
+                if cost < best_cost:
+                    best_cost = cost
+                    best_weights = self.weights
+                
+                print(f"{epoch=}, and {cost=}")
+                epoch += 1
+        except KeyboardInterrupt:
+            print(f"best cost function value is {best_cost}, and the weights are {best_weights}")
+        """
+
+    def _gradient(self) -> np.ndarray[float]:
+        gradient = np.zeros(self.size)
+
+        for weight_index, _ in enumerate(gradient):
+            gradient[weight_index] = self._partial_derivative(weight_index)
+
+        return gradient
+
+    def _cost_function(self) -> float:
+        mean_squared_error = 0
+
+        for index, row in enumerate(self.X_data):   # iterate over all the rows in the dataset
+            term = self.weights[0]
+
+            for i, x in enumerate(row[1:], start=1):    # iterate over all the columns in the row
+                term += self.weights[i] * x
+
+            mean_squared_error += (term - self.y_data[index]) ** 2
+
+        mean_squared_error = mean_squared_error / len(self.y_data)
+
+        return mean_squared_error ** 0.5
+    
+
+def generate_random_data(SEED: int) -> tuple[np.ndarray, np.ndarray]:
+    np.random.seed(SEED)
+
+    X_data = 2 * np.random.rand(SIZE, 1)    # with only one variable
+    y_data = 4 + 3 * X_data + np.random.randn(SIZE, 1)
+
+    return X_data, y_data
+
+
+def main() -> int:
+    X_data, y_data = generate_random_data(SEED)
+
+    linear_regression = LinearRegression(eta=LEARNING_RATE, epochs=EPOCHS)
+    linear_regression.fit(X_data, y_data)
+
+    print(linear_regression.weights_)
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
---
+++ logistic_regression.py
@@ -0,0 +1,121 @@
+"""
+Implementing Logistic Regression in python using numpy and scikit-learn for metrics and data, DataFrame from pandas
+"""
+from __future__ import annotations
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+import numpy as np
+from math import log
+from typing import (
+    Self,
+)
+
+
+ALPHA: float = 0.2    # learning rate, here it is called alpha
+SEED: int = 42
+EPOCHS: int = 1000
+THRESHOLD: float = 0.5
+
+
+class LogisticRegression(object):
+    def __init__(self, alpha: float = ALPHA, threshold: float = THRESHOLD, epochs: int = EPOCHS) -> None:
+        self.ALPHA = alpha
+        self.EPOCHS = epochs
+        self.THRESHOLD = threshold
+
+    def fit(self, X_data: np.ndarray, y_data: np.ndarray) -> Self:
+        self.X_data = X_data
+        self.y_data = y_data
+        self.NUMBER_OF_WEIGHTS = len(X_data[0]) + 1
+        self.weights = np.random.randn(self.NUMBER_OF_WEIGHTS)
+        self._gradient_descent()
+        return self
+
+    @property
+    def weights_(self) -> np.ndarray[float]:
+        return self.weights.copy()
+
+    def _gradient_descent(self) -> None:
+        for epoch in range(self.EPOCHS):
+            self.weights = self.weights - self.ALPHA * self._gradient()
+            # print(f"{epoch=}, cost = {self._cost_function()}")
+
+    def predict_probability(self, x_vector: np.ndarray) -> float:
+        probability = self.weights[0]
+
+        for index, x in enumerate(x_vector, start=1):
+            probability += self.weights[index] * x
+
+        return (probability := self.sigmoid(probability))
+
+    def predict_class(self, x_vector: np.ndarray) -> int:
+        probability = self.predict_probability(x_vector)
+        return 1 if probability >= self.THRESHOLD else 0
+
+    def _partial_derivative(self, weight_index: int) -> float:
+        derivative = 0
+
+        for index, row in enumerate(self.X_data):
+            CO_EFFICIENT = 1 if weight_index == 0 else row[weight_index - 1]
+            term = self.weights[0]
+
+            for i, x in enumerate(row, start=1):
+                term += self.weights[i] * x
+
+            term = self.sigmoid(term)
+            term -= self.y_data[index]
+            term *= CO_EFFICIENT
+            derivative += term
+
+        return derivative / len(self.y_data)
+
+    def _gradient(self) -> np.ndarray[float]:
+        return np.array([self._partial_derivative(weight_index) for weight_index in range(self.NUMBER_OF_WEIGHTS)])
+
+    def _cost_function(self) -> float:      # this is the log loss cost function
+        log_loss = 0
+
+        for index, row in enumerate(self.X_data):
+            probability = self.predict_probability(row)
+            log_loss += self.y_data[index] * log(probability) + (1 - self.y_data[index]) * log(1 - probability)
+
+        log_loss = -1 / len(self.y_data) * log_loss
+
+        return log_loss
+
+    @staticmethod
+    def sigmoid(x: float) -> float:
+        return 1 / (1 + (np.e ** -x))
+
+
+def generate_data(seed: int | None = None) -> tuple[np.ndarray, np.ndarray]:
+    if seed is None:
+        seed = 42
+
+    np.random.seed(seed)
+    dataset = load_iris(as_frame=True)
+    X_data = dataset.data[["petal width (cm)", "sepal length (cm)", "sepal width (cm)", "petal length (cm)"]].values
+    y_data = dataset.target_names[dataset.target] == "virginica"
+    X_data = np.array(X_data)
+    y_data = np.array(y_data, dtype=np.int16)   # convert the boolean values to int
+
+    return X_data.copy(), y_data.copy()
+
+
+def main() -> int:
+    for seed in range(42, 50):
+        X_data, y_data = generate_data(seed)
+        X_data_train, X_data_test, y_data_train, y_data_test = train_test_split(X_data, y_data, random_state=42)
+
+        logistic_regression = LogisticRegression(alpha=ALPHA, threshold=THRESHOLD, epochs=EPOCHS)
+        logistic_regression = logistic_regression.fit(X_data_train, y_data_train)
+
+        print(f"confusion matrix = {confusion_matrix(y_data_test, [logistic_regression.predict_class(row) for row in X_data_test])}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
