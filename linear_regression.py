a009a4a66c92d85a1a7c5b77a498c1f35eb8da2a
souravauddy <souravauddy25@gmail.com>
2023-07-02 23:28:02 +0530

    making some changes to linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 import numpy as np
+import sys
 from typing import (
     Self, 
     Final
 )
 from numpy.typing import (
-    NDArray
+    NDArray,
 )
 
 
@@ -24,23 +25,62 @@ EPOCHS: Final[int] = 1000
 
 
 class LinearRegression(object):
-    def __init__(self, *, eta: float = LEARNING_RATE, epochs: int = EPOCHS) -> None:
+    def __init__(self, *, eta: float = LEARNING_RATE, epochs: int | None = EPOCHS) -> None:
+        """
+        takes input the hyperparameters learning rate and the number of epochs
+
+        Args:
+            eta (float, optional): learning rate given as float. Defaults to LEARNING_RATE.
+            epochs (int | None, optional): no of epochs given as an integer, if None is given then unlimited no of epochs is performed, stops using Keyboard Interrupt. Defaults to EPOCHS.
+        """
+
         self.LEARNING_RATE = eta
         self.EPOCHS = epochs
 
-    def fit(self, X_data: np.ndarray, y_data: np.ndarray) -> Self:
+    def fit(self, X_data: NDArray, y_data: NDArray) -> Self:
+        """
+        Uses gradient descent to find the optimal weights, and returns and instance of itself.
+
+        Args:
+            X_data (NDArray): data of the input features
+            y_data (NDArray): data of the target features
+
+        Returns:
+            Self: returns an instance of itself after computing the weights for the sake of compatibility with sklearn.
+        """
+
         self.X_data = X_data
         self.y_data = y_data
         self.size = len(X_data[0]) + 1
         self.weights = np.random.randn(self.size)   # intialze with some random weights
-        self._gradient_descent()
+
+        if self.EPOCHS is not None:
+            self._gradient_descent()
+        else:
+            self._gradient_descent(unlimited_epcohs=True)
+
         return self
     
     @property
     def weights_(self) -> np.ndarray[tuple[int, int], np.dtype[np.float64]]:
+        """returns the optimal weights, should be called after calling the fit method.
+
+        Returns:
+            np.ndarray[tuple[int, int], np.dtype[np.float64]]: returns the weights as a numpy array.
+        """
+
         return self.weights.copy()
     
     def _partial_derivative(self, weight_index: int) -> float:
+        """computes the partial derivative of a single vector and returns its value.
+
+        Args:
+            weight_index (int): The index of the vector whose derivative is to be computed.
+
+        Returns:
+            float: computed parial derivative.
+        """
+
         root_mean_squared_error = self._cost_function()
         derivative = 0
 
@@ -57,13 +97,20 @@ class LinearRegression(object):
 
         return (derivative / len(self.y_data)) / root_mean_squared_error
 
-    def _gradient_descent(self) -> None:
-        for epoch in range(self.EPOCHS):
-            self.weights = self.weights - self.LEARNING_RATE * self._gradient()
-            print(f"{epoch=}, {self.weights=}")
+    def _gradient_descent(self, unlimited_epcohs: bool = False) -> None:
+        """performs gradient descent.
 
+        Args:
+            unlimited_epcohs (bool, optional): for unlimited epcohs, using keyboard interrupt. Defaults to False.
         """
-        # another approach using keyboard interupt for unlimited epochs
+        
+        if not unlimited_epcohs:
+            for epoch in range(self.EPOCHS):
+                self.weights = self.weights - self.LEARNING_RATE * self._gradient()
+                print(f"{epoch=}, {self.weights=}")
+            
+            return
+
         best_weights = None
         best_cost = sys.maxsize
         epoch = 1
@@ -80,10 +127,17 @@ class LinearRegression(object):
                 print(f"{epoch=}, and {cost=}")
                 epoch += 1
         except KeyboardInterrupt:
+            assert best_weights is not None
             print(f"best cost function value is {best_cost}, and the weights are {best_weights}")
-        """
+
 
     def _gradient(self) -> np.ndarray[tuple[int], np.dtype[np.float64]]:
+        """returns the gradient vector, with all the computed parital derivatives.
+
+        Returns:
+            np.ndarray[tuple[int], np.dtype[np.float64]]: gradient vector.
+        """
+
         gradient = np.zeros(self.size)
 
         for weight_index, _ in enumerate(gradient):
@@ -92,6 +146,12 @@ class LinearRegression(object):
         return gradient
 
     def _cost_function(self) -> float:
+        """Cost function for calculating the loss. Here, we used Root Mean Squared Error.
+
+        Returns:
+            float: cost
+        """
+
         mean_squared_error = 0.0
 
         for index, row in enumerate(self.X_data):   # iterate over all the rows in the dataset
@@ -107,8 +167,17 @@ class LinearRegression(object):
         return mean_squared_error ** 0.5
     
 
-def generate_random_data(SEED: int) -> tuple[np.ndarray[tuple[int, int], np.dtype[np.float64]], np.ndarray[tuple[int, int], np.dtype[np.float64]]]:
-    np.random.seed(SEED)
+def generate_random_data(seed: int) -> tuple[np.ndarray[tuple[int, int], np.dtype[np.float64]], np.ndarray[tuple[int, int], np.dtype[np.float64]]]:
+    """generates random data for linear regression, using the SEED parameter.
+
+    Args:
+        seed (int): value to reproduce the results
+
+    Returns:
+        tuple[np.ndarray[tuple[int, int], np.dtype[np.float64]], np.ndarray[tuple[int, int], np.dtype[np.float64]]]: two numpy arrays, input features and output column
+    """
+
+    np.random.seed(seed)
 
     X_data = 2 * np.random.rand(SIZE, 1)    # with only one variable
     y_data = 4 + 3 * X_data + np.random.randn(SIZE, 1)
